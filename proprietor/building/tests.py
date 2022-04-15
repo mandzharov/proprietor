a8ab4e888d838ce387a2a6d3a97d8308a8670856
Milen Mandzharov <milen.mandzharov@gmail.com>
2022-04-15 13:29:08 +0300

    report view tests

--- proprietor/building/tests.py
+++ proprietor/building/tests.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import Permission
 from django.urls import reverse
-from proprietor.building.models import Apartment, Building
+from proprietor.building.models import Apartment, Building, UtilitiesExpenses, UtilityType
 
 
 UserModel = get_user_model()
@@ -35,6 +35,22 @@ APARTMENT_2_DATA = {
     "area": 64.5,
     "rooms_count": 2,
     "share": 8.23,
+}
+
+EXPENSE_1_DATA = {
+    'utility_type': '',
+    'amount': 12.8,
+    'bill_year': 2022,
+    'bill_month': 'JAN',
+    'apartment': '',
+}
+
+EXPENSE_2_DATA = {
+    'utility_type': '',
+    'amount': 13.2,
+    'bill_year': 2022,
+    'bill_month': 'JAN',
+    'apartment': '',
 }
 
 
@@ -128,3 +144,69 @@ class AddApartmentViewTest(TestCase):
         self.assertTrue(self.user_1.has_perm('building.add_apartment'))
         response = self.client.get(reverse('create apartment', kwargs={'pk': self.building.pk}))
         self.assertEqual(403, response.status_code)
+
+
+class ReportViewTest(TestCase):
+    def setUp(self):
+        self.user_1 = UserModel.objects.create_user(email=USER_1_DATA["email"])
+        self.user_1.set_password(USER_1_DATA["password"])
+        self.user_1.save()
+
+        self.user_2 = UserModel.objects.create_user(email=USER_2_DATA["email"])
+        self.user_2.set_password(USER_2_DATA["password"])
+        self.user_2.save()
+
+        self.building = Building.objects.create(**BUILDING_DATA)
+        self.building.save()
+
+        APARTMENT_1_DATA.update({"building": self.building})
+        self.apartment_1 = Apartment.objects.create(**APARTMENT_1_DATA)
+        self.apartment_1.owner.add(self.user_1)
+        self.apartment_1.save()
+
+        APARTMENT_2_DATA.update({"building": self.building})
+        self.apartment_2 = Apartment.objects.create(**APARTMENT_2_DATA)
+        self.apartment_2.owner.add(self.user_2)
+        self.apartment_2.save()
+
+        self.expense_type_water = UtilityType.objects.create(name='Water', description='')
+        self.expense_type_water.save()
+        self.expense_type_electricity = UtilityType.objects.create(name='Electricity', description='')
+        self.expense_type_electricity.save()
+        self.expense_type_heating = UtilityType.objects.create(name='Heating', description='')
+        self.expense_type_heating.save()
+
+    def test_report_avg_calc(self):
+        permission = Permission.objects.get(name='Can view utilities expenses')
+        self.user_1.user_permissions.add(permission)
+        self.client.login(**USER_1_DATA)
+
+        EXPENSE_1_DATA["utility_type"] = self.expense_type_electricity
+        EXPENSE_1_DATA["apartment"] = self.apartment_1
+        expense_1 = UtilitiesExpenses.objects.create(**EXPENSE_1_DATA)
+        expense_1.save()
+
+        EXPENSE_2_DATA["utility_type"] = self.expense_type_heating
+        EXPENSE_2_DATA["apartment"] = self.apartment_1
+        expense_2 = UtilitiesExpenses.objects.create(**EXPENSE_2_DATA)
+        expense_2.save()
+
+        response = self.client.get(reverse('report', kwargs={'apt_pk': self.apartment_1.pk}))
+        self.assertEqual(200, response.status_code)
+        self.assertEqual(13, response.context['result'])
+
+    def test_view_not_own_report_403(self):
+        permission = Permission.objects.get(name='Can view utilities expenses')
+        self.user_1.user_permissions.add(permission)
+        self.client.login(**USER_1_DATA)
+
+        response = self.client.get(reverse('report', kwargs={'apt_pk': self.apartment_2.pk}))
+        self.assertEqual(403, response.status_code)
+
+    def test_view_report_not_existing_apt_404(self):
+        permission = Permission.objects.get(name='Can view utilities expenses')
+        self.user_1.user_permissions.add(permission)
+        self.client.login(**USER_1_DATA)
+
+        response = self.client.get(reverse('report', kwargs={'apt_pk': 30}))
+        self.assertEqual(404, response.status_code)
